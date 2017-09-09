ee64d9ebcdf2c1d69eccb04f0b2dd3388f677bec
AhmadHani <dr.dp2007@gmail.com>
2017-09-09 16:59:15 +0200

    add admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,12 +39,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-        ],
-            'admin'=> [
-    'admin'=> \App\Http\Middleware\IsAdmin::class
-    
-            ]
-    
+        ]
     ];
 
     /**
@@ -60,5 +55,9 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class];
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'admin'=> \App\Http\Middleware\isAdmin::class
+    
+
+        ];
 }
