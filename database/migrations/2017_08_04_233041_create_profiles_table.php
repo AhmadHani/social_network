5fc4f73b832d2a8b0253f5bbfb296567eadbf3eb
AhmadHani <dr.dp2007@gmail.com>
2017-09-09 16:46:34 +0200

    edit migrate file

--- database/migrations/2017_08_04_233041_create_profiles_table.php
+++ database/migrations/2017_08_04_233041_create_profiles_table.php
@@ -15,7 +15,7 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->increments('id');
-            $table->boolean("user_id");
+            $table->integer("user_id");
             $table->string("location")->nullable();
             $table->text("about")->nullable();
             $table->date("date_birth")->nullable();
